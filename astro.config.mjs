fe5aff281b9f774c1cc3dd3a16a1290b385868e0
almahmud <13126441+almahmudbd@users.noreply.github.com>
2026-05-14 12:09:36 +0600

    chore: rename pages and setup redirects for github/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://almahmudbd.github.io/',
+  redirects: {
+    '/consulting': '/appointment',
+    '/blog': '/social',
+    '/serial': '/appointment',
+    '/consult': '/appointment',
+    '/raqilist': 'https://amarbokbok.vercel.app/blogs/raqilist',
+    '/fb': 'https://www.facebook.com/almahmud1234',
+    '/page': 'https://www.facebook.com/thealmahmud2',
+    '/yt': 'https://youtube.com/c/thealmahmud',
+  }
 });
